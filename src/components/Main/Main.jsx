e5dda29f004ad23091aac101e5f2139499a46211
Ulunji Felix Mkandawire <designedbyulunji@gmail.com>
2024-06-06 16:21:08 +0200

    Add favicon and complete main section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,18 @@ import "../../../universal-styles.scss";
 import "./main.scss";
 
 function Main() {
-	return <div className="app-main">hello</div>;
+	return (
+		<div className="outer__main-try">
+			<div className="app-main">
+				<h2 className="hero__text">
+					<small>Mbumba Car Hire</small>
+					<br />
+					Luxury Car Rentals for Every Occasion
+				</h2>
+				<button className="btn btn__start">Get Started</button>
+			</div>
+		</div>
+	);
 }
 
 export default Main;
